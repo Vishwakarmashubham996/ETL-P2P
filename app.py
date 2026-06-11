66e5e54bc6a600c9531a113b3d3ec32b0fa2e80a
Vishwakarmashubham996 <shubhamv.ey@gmail.com>
2026-06-11 11:49:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+
+import streamlit as st
+st.set_page_config(page_title='EY SAP P2P ETL Studio', layout='wide')
+st.title('EY SAP P2P ETL Studio')
+st.markdown('Migration utility for SAP HANA → SQL Server')
